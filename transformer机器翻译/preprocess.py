de376371db0c4d48c42e5e188a9c1b56f76b881f
fangcheng <orangerfun@gmail.com>
2019-11-02 16:44:50 +0800

    完善数据预处理程序

--- transformer机器翻译/preprocess.py
+++ transformer机器翻译/preprocess.py
@@ -1,10 +1,20 @@
+# -*- coding:utf-8 -*-
+
 """
 数据预处理
+主要流程：
+	1.按照词频创建词汇文件，格式：词：词频
+	2.从词汇文件中读取词，并建立词汇到索引(索引到词汇)的映射
+	3.根据映射将文本转换成向量形式(数字形式)
+	4.生成一个batch的训练和测试数据
+@author:orangerfun@gmail.com
 """
+
 import codecs
 import re
 from collections import Counter
 import numpy as np
+import tensorflow as tf
 
 np.set_printoptions(threshold = np.inf)
 
@@ -57,26 +67,28 @@ def sentence2id(sourcefile, targetfile):
 	return x_list,y_list
 
 def load_train_data():
+	"""生成训练数据"""
 	ch_sents = [re.sub(r"[,.\[\]'?|\"-()，。‘’“”？：；;:<>《》]", "", line) for line in codecs.open(ch_train, "r", "utf-8").readlines() if line.strip()]
 	en_sents = [re.sub(r"[,.\[\]'?|\"-()，。‘’“”？：；;:<>《》]", "", line) for line in codecs.open(en_train, "r", "utf-8").readlines() if line.strip()]
 	x,y = sentence2id(ch_sents, en_sents)
 	return x,y
 
+def load_test_data():
+	"""生成测试数据"""
+	ch_sents = [re.sub(r"[,.\[\]'?|\"-()，。‘’“”？：；;:<>《》]", "", line) for line in codecs.open(ch_test, "r", "utf-8").readlines() if line.strip()]
+	en_sents = [re.sub(r"[,.\[\]'?|\"-()，。‘’“”？：；;:<>《》]", "", line) for line in codecs.open(en_test, "r", "utf-8").readlines() if line.strip()]
+	x, y = sentence2id(ch_sents, en_sents)
+	return x,y
 
-
-
-
-
-
-
-
-
+def get_batch_data():
+	"""生成一个batch的数据"""
+	x, y = load_train_data()
+	num_batch = len(x)//batchsize
+	x, y = tf.convert_to_tensor(x), tf.convert_to_tensor(y)
+	input_queues = tf.train.slice_input_producer([x,y])
+	x,y = tf.train.shffle_batch(input_queues, batch_size=batchsize, num_threads=8, capacity=batchsize*64, min_after_dequeue=batchsize*32)
+	return x, y, num_batch
 
 if __name__ == '__main__':
-	# make_vocab(ch_train, "ch_words.txt")
-	# make_vocab(en_train, "en_words.txt")
-	x,y = load_train_data()
-	print(x[:30])
-	print("======")
-	print(y[:30])
-	print(x.shape,y.shape)
+	make_vocab(ch_train, "ch_words.txt")
+	make_vocab(en_train, "en_words.txt")
